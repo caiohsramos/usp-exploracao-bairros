9859188c73b7dd71c3fde075a7af4c910e4a8d92
Julio Kenji Ueda <julio.ueda@usp.br>
2017-05-14 00:10:26 -0300

    criação de user_data e testes de renderização de informações do Google Maps API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :userdata
   root to: 'visitors#index'
   devise_for :users
   resources :users
-end
+
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224203311) do
+ActiveRecord::Schema.define(version: 20170513175237) do
+
+  create_table "userdata", force: :cascade do |t|
+    t.string   "nome"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
