bca0d725b6e864148d415079ed5bdc951d7af591
lbenicio <leonardo.abenicio@gmail.com>
2017-05-19 15:00:37 -0300

    change seach form to post instead of get

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
     root to: 'visitors#index'
     devise_for :users
     resources :users
+    
+    post "/search/" => "search#index"
 
     resources :neighborhoods
     resources :userdata
     resources :search
     
-    get "/search/show" => "search#show"
+    post "/search/show" => "search#show"
+   
   end
